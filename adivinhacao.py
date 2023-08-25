8038da2f9a43806bee18e812039fc101e9f9b008
Eduardo Toste <duciochetti@gmail.com>
2023-08-25 08:55:00 -0300

    Sequencia nos estudos

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,11 +6,15 @@ numero_secreto = 42
 rodada = 1
 tentativas = 3
 
-while (rodada <= tentativas):
+for rodada in range (1, tentativas + 1):
     print("Tentativa {} de {}" .format(rodada, tentativas))
-    chute_str = input("Digite seu numero:")
+    chute_str = input("Digite seu numero entre 1 e 100:")
     print("Voce digitou:",chute_str)
     chute   = int(chute_str)
+
+    if(chute < 1 and chute > 100):
+        print("Voce deve digitar um numero de 1 a 100")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -18,11 +22,11 @@ while (rodada <= tentativas):
 
     if(acertou):
         print("Voce acertou!")
+        break
     else:
         if(maior):
             print("Voce errou! Chutou acima do valor...")
         elif(menor):
             print("Voce errou! Chutou abaixo do valor...")
 
-    rodada += 1
     print("Fim do jogo.")
